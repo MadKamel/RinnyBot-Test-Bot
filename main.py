77364563e0107da55ad9fc78a8a4f39b6229ec94
Eric Rustrum <rustrumek@gmail.com>
2021-01-05 08:47:57 -0800

    Commit latest code.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # ( ͡° ͜ʖ ͡°)
 
-import discord
+
 print('''
 .________________________________________________________.
 |                                                        |
@@ -12,13 +12,18 @@ print('''
 
 RinnyBot is starting...''')
 
+
+
 import discord, os
 token = open('.token').read()
+print('Token read, initializing the bot.')
+print('Token is (' + token + ')')
 
 # Init Discord Bot Client, with full intents.
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 
+print('Bot initialized, loading code.')
 
 @client.event
 async def on_ready():
@@ -27,7 +32,7 @@ async def on_ready():
     LogChan = loadchan(783249066834264065) #Rinnydev logging channel
     #LogChan = loadchan(720701116127510589) # Rinnyverse logging channel
 
-    print('BreenBot is active.')
+    print('RinnyBot is active.')
 
 
 
@@ -36,12 +41,14 @@ async def on_message(msg):
     if msg.content[0] == '>':
         try:
             if msg.content.split(' ')[0] == '>kick':
-
                 # This line means to kick a user by given ID.
                 # example: >kick 433433822248304641
-                loadguild(msg.channel.guild.id).kick(loadmember(int(msg.content.split(' ')[1], msg.channel.guild.id)
-        await msg.delete()
-    pass #TODO: add chat filter for N-Word
+                loadguild(msg.channel.guild.id).kick(loadmember(int(msg.content.split(' ')[1]), msg.channel.guild.id))
+                await msg.delete()
+        except:
+           pass
+    if 'nigger' in str(msg.content).split(' '):
+        loadguild(msg.channel.guild.id).kick(loadmember(int(msg.author.id), msg.channel.guild.id))
 
 
 
@@ -59,28 +66,30 @@ async def on_message_delete(msg):
 
 
 def loadchan(id): # Loads a channel
-  global client
-  print('Channel #' + str(client.get_channel(id)) + ' loaded.')
-  return client.get_channel(id)
+    global client
+    print('Channel #' + str(client.get_channel(id)) + ' loaded.')
+    return client.get_channel(id)
 
 def loadrole(guild, id): # Loads a role from a specific guild
-  print('Role <' + str(guild.get_role(id)) + '> loaded.')
-  return guild.get_role(id)
+    print('Role <' + str(guild.get_role(id)) + '> loaded.')
+    return guild.get_role(id)
 
 def loadguild(id): # Loads a guild (server)
-  global client
-  print('Guild ' + str(client.get_guild(id)) + ' loaded.')
-  return client.get_guild(id)
+    global client
+    print('Guild ' + str(client.get_guild(id)) + ' loaded.')
+    return client.get_guild(id)
 
 def loadmember(guild, id): # Loads a member from an id
-  print('User @' + str(guild.get_member(id)) + ' loaded.')
-  return guild.get_member(id)
+    print('User @' + str(guild.get_member(id)) + ' loaded.')
+    return guild.get_member(id)
 
 async def setstatus(activity):
-  global client
-  print('Setting status to: ' + activity)
-  await client.change_presence(status=discord.Status.online, activity=discord.Game(activity))
+    global client
+    print('Setting status to: ' + activity)
+    await client.change_presence(status=discord.Status.online, activity=discord.Game(activity))
 
 
+
+print('Running RinnyBot...')
 
 client.run(token)
